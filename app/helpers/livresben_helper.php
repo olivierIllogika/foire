7d972608126d0df8fe89a876f85cfcb2e8bf099f
Olivier Martin <olivier2.martin@gmail.com>
2007-01-17 12:19:59 +0000

    logging/emailing when giving back money/losing books

--- app/helpers/livresben_helper.php
+++ app/helpers/livresben_helper.php
@@ -40,6 +40,44 @@ class LivresbenHelper extends AppController
     }
     return $text;
   }
+  
+
+  function courrielLivresVendus($email, $bookList, $returnedBooks, $returnedMoney)
+  {
+
+    $body =
+    "Vous venez de récupérer $returnedMoney $ de la vente de vos livres.  ".
+    "Cette somme correspond au total de vos ventes moins le montant de la commission prélevée.\n\n".
+    "Voici le détail de vos livres vendus :\n\n".
+    $bookList.
+
+    "\n\nMerci d'avoir participé à cette édition de la Foire !";
+
+    sendSMTP($email,'','','[Foire]Livres vendus', $body, false,'Foire aux Livres <foire-noreply@step.polymtl.ca>');
+
+  }  
+  
+  function logLostBooks($etudiantId, $etudiantNom)
+  {
+
+    // look for books en_consigne and not part of a bill    
+    $ret = $this->models['livre']->findBySql("SELECT id, titre, prix FROM livres AS l LEFT JOIN facture_lignes AS fl ON fl.livre_id=l.id WHERE en_consigne=1 AND ISNULL(livre_id) AND codebar=$etudiantId ");
+    
+    if ($ret)
+    {
+        // save a file with the data from student
+        $sqlBooks = '';
+        foreach($ret as $row)
+        {
+            $sqlBooks .= ($sqlBooks ? ',' : '').$row['id'];
+        }
+        $sqlBooks = "UPDATE livres SET codebar=$etudiantId WHERE id IN ($sqlBooks);";
+        //$this->print_pre($bookList,true);
+        
+        $this->log("perdus $etudiantNom / ".$sqlBooks);
+    }
+            
+  }
 }
 
 ?>
