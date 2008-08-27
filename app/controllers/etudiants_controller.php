4cd4665b59aee09d8ca7f61f494d6792c3cc0965
Olivier Martin <olivier2.martin@gmail.com>
2008-08-27 12:45:53 +0000

    condition looking for previous attempt at getting personal info back was wrong

--- app/controllers/etudiants_controller.php
+++ app/controllers/etudiants_controller.php
@@ -197,7 +197,7 @@ class EtudiantsController extends EtudiantsHelper
 //      $ret = $this->models['etudiant']->findAll(
 //          "courriel='{$this->params['data']['courriel']}'",
 //          array('id','nom','prenom','courriel'));
-     $ret = $this->models['etudiant']->findBySql("SELECT e.id, nom, prenom, courriel, TIME_TO_SEC(TIMEDIFF(NOW(), eve.created)) AS lastrequest FROM etudiants AS e LEFT JOIN evetudiants AS eve ON e.id=eve.id ".
+     $ret = $this->models['etudiant']->findBySql("SELECT e.id, nom, prenom, courriel, TIME_TO_SEC(TIMEDIFF(NOW(), eve.created)) AS lastrequest FROM etudiants AS e LEFT JOIN evetudiants AS eve ON e.id=eve.id and evenement=405 ".
      "WHERE (evenement=405 OR ISNULL(evenement)) AND courriel='{$this->params['data']['courriel']}' ORDER BY eve.created DESC LIMIT 1");
           
       if ($ret)
